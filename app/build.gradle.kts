97f5d2a4fe5566fa4e37e11872322acc297622bd
ibrah444 <ibrah444@gmail.com>
2024-08-13 03:29:22 +0200

    Update build.gradle.kts
    Added dependencies to allow authentication by phone number:
    dependencies {
        // Import the BoM for the Firebase platform
        implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
    
        // Add the dependency for the Firebase Authentication library
        // When using the BoM, you don't specify versions in Firebase library dependencies
        implementation("com.google.firebase:firebase-auth")
    }

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,3 +101,12 @@ dependencies {
 }
 
 apply(plugin = "com.google.gms.google-services")
+
+dependencies {
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+}
